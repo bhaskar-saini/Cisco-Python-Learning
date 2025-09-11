7eba5d4e9c7c68c34e31871eb5bd85b616e4d9a9
Bhaskar Saini <bhaskarsaini333@gmail.com>
2025-09-11 16:41:26 +0530

    Day3

---
+++ day3/emp_app_sql_dict_rich/exc.py
@@ -0,0 +1,12 @@
+
+class EmployeeException(Exception):
+    pass 
+
+class EmployeeNotFoundError(EmployeeException):
+    pass 
+
+class EmployeeAlreadyExistError(EmployeeException):
+    pass 
+
+class DatabaseError(EmployeeException):
+    pass 
